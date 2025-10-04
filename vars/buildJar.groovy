36f7b0e63af71ebc876536b739f37169743496cd
Marselous <stanislav.vainer@outlook.com>
2025-10-04 02:23:40 +0100

    syntax fix

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,10 +1,6 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "...building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t justfreak/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push justfreak/demo-app:jma-2.0'
-    }
+    echo 'building the applications...'
+    sh 'mvn package'
 }
