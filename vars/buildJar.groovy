eaf87ed1c4c7a7577933ce3776bb2194d47f8947
Marselous <stanislav.vainer@outlook.com>
2025-10-04 01:54:56 +0100

    adding docker build/push to buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,10 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "building the application for branch $GIT_BRANCH"
-    sh 'mvn package'
+    echo "...building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t justfreak/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push justfreak/demo-app:jma-2.0'
+    }
 }
