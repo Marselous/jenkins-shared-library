0ed200c55338fbbced8494c94b4b128cbc03202b
Marselous <stanislav.vainer@outlook.com>
2025-10-06 15:57:56 +0100

    update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,17 @@
 #!/user/bin/env groovy
-
 import com.example.Docker
+
 def call(String imageName) {
-    return new Docker(this).with {
-        dockerLogin()
-        buildDockerImage(imageName)
-        dockerPush(imageName)
-    }
+    def docker = new Docker(this)
+    docker.dockerLogin()
+    docker.buildDockerImage(imageName)
+    docker.dockerPush(imageName)
 }
+
+// def call(String imageName) {
+//     return new Docker(this).with {
+//         dockerLogin()
+//         buildDockerImage(imageName)
+//         dockerPush(imageName)
+//     }
+// }
